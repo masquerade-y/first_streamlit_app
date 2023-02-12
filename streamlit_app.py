a591e61d968a24a55e663f3408e653e048ba6b65
YZ <60045730+masquerade-y@users.noreply.github.com>
2023-02-11 18:24:50 -0600

    Update streamlit_app.py
    added a multiselect object for the table

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # import data
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-# display data
+# reset index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display data as a table on the page
 streamlit.dataframe(my_fruit_list)
